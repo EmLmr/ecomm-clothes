d04113dbac4648d6a27f97b98ccc5641e813bcf1
EmLmr <emeline.coding@gmail.com>
2022-05-18 16:45:30 +0200

    Added text and image to slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -31,6 +31,47 @@ const Arrow = styled.div`
     margin: auto;
 `;
 
+const Wrapper = styled.div`
+    height: 100%;
+`;
+
+// Slider content
+const Slide = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+`;
+
+const ImgContainer = styled.div`
+    flex: 1;
+    height: 100%;
+`;
+
+const Image = styled.img`
+    height: 80%;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 4rem;
+`;
+
+const Title = styled.h1`
+    /* font-size: 70px; */
+    font-size: 4rem;
+`;
+const Description = styled.h2`
+    margin: 4rem 0;
+    letter-spacing: 2px;
+`;
+const Button = styled.button`
+    padding: 10px;
+    font-size: 1.5rem;
+    background-color: transparent;
+    cursor: pointer;
+`;
+
 const Slider = () => {
     return (
         <Container>
@@ -39,6 +80,21 @@ const Slider = () => {
                 {/*passing props to use for styling*/}
                 <ArrowBackIosNewIcon fontSize="small" />
             </Arrow>
+
+            <Wrapper>
+                <Slide>
+                    <ImgContainer>
+                        <Image alt="Some image" />
+                    </ImgContainer>
+
+                    <InfoContainer>
+                        <Title>SUMMER SALE</Title>
+                        <Description>Don't compromise on style! Get 30% off on new arrivals.</Description>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+            </Wrapper>
+
             <Arrow direction="right">
                 <ArrowForwardIosIcon fontSize="small" />
             </Arrow>
